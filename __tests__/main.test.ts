627ec056e433629bea73f90276a325494cdf9ea4
Lucas <lucas_klasa@hotmail.com>
2023-02-21 22:29:24 -0300

    chore: add semi rule

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest';
-import { greeter } from "../src/main.js";
+import { describe, it, expect } from 'vitest'
+import { greeter } from "../src/main.js"
 
 describe('greeter function', () => {
   // Assert greeter result
   it('greets a user with `Hello, {name}` message', async () => {
-    const name = 'john';
-    const result = await greeter(name);
-    expect(result).toBe(`Hello, ${name}`);
-  });
-});
+    const name = 'john'
+    const result = await greeter(name)
+    expect(result).toBe(`Hello, ${name}`)
+  })
+})
